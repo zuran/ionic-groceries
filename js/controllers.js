b03532201a42853e79c237183201eb978a071711
flowmancer <notquitenoon@gmail.com>
2015-06-24 21:21:48 -0400

    Edit items with long press

--- js/controllers.js
+++ js/controllers.js
@@ -12,20 +12,27 @@
           inactiveList: []
         };
 
-        if (!$localstorage.getObject('groceries') || !$localstorage.getObject('groceries').activeList) {
+        if (!$localstorage.getObject('groceries') ||
+            !$localstorage.getObject('groceries').activeList) {
+          $localstorage.setObject('groceries', $scope.vm);
+        }
+        
+        if ($localstorage.getObject('groceries').activeList.length === 0) {
+          $scope.vm.activeList = [
+            {name: 'Apples', price: 5.99, quantity: 2},
+            {name: 'Bananas', price: 0.99, quantity: 3},
+            {name: 'Bread', price: 1.89, quantity: 1},
+            {name: 'Carrots', price: 2.50, quantity: 1}
+            
+          ];
+          $scope.vm.inactiveList = [
+            {name: 'Ketchup', price: 2.34, quantity: 1},
+            {name: 'Tofu', price: 2.99, quantity: 1}
+          ];
           $localstorage.setObject('groceries', $scope.vm);
         }
         
         $scope.vm = $localstorage.getObject('groceries');
-        
-//        (function init() {
-//          $scope.vm.inactiveList.push({
-//            name: 'Juice',
-//            quantity: 1,
-//            price: 2.89
-//          });
-//          $localstorage.setObject('groceries', $scope.vm);
-//        }());
         
         function getTotal() {
           var total = 0;
@@ -99,6 +106,47 @@
             ]
           });
         };
+        
+        $scope.editClicked = function (index, list) {
+          $scope.newItem = {
+            name: list[index].name,
+            quantity: list[index].quantity,
+            price: list[index].price
+          };
+          
+          
+          
+          var popup = $ionicPopup.show({
+            template: '<label class="item item-input"><span class="input-label">Item Name</span><input ng-model="newItem.name' +
+              '" type="text"></label><label class="item item-input"><span class="input-label">Price</span><input ng-model="newItem.price' +
+              '" type="number"></label>',
+            title: 'Edit Item',
+            scope: $scope,
+            buttons: [
+              {
+                text: 'Save',
+                type: 'button-positive',
+                onTap: function (e) {
+                  if (!$scope.newItem.name || !$scope.newItem.price) {
+                    e.preventDefault();
+                  } else {
+                    list.splice(index, 1);
+                    list.push($scope.newItem);
+                    if (list === $scope.vm.activeList) {
+                      $scope.vm.activeList = $filter('orderBy')(list, 'name');
+                    } else { $scope.vm.inactiveList = $filter('orderBy')(list, 'name'); }
+                    $scope.vm.total = getTotal();
+                    $localstorage.setObject('groceries', $scope.vm);
+                    return $scope.newItem;
+                  }
+                }
+              },
+              { text: 'Cancel' }
+            ]
+          });
+        };
+        
+        
         
       }]);
 }());
